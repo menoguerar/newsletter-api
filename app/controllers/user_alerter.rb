d75d467bcd2e28fd6bc82cf340e948805ed944d5
Marlon Enrique Noguera Ramirez <menoguerar@unal.edu.co>
2018-10-24 15:03:47 -0500

    email

--- app/controllers/user_alerter.rb
+++ app/controllers/user_alerter.rb
@@ -10,20 +10,20 @@ class UserAlerter< ApplicationController
             puts @topic.inspect
             @users_of_topic = @topic.users
 
-            @user=User.find(12)
-            SendEmailJob.set(wait: 1.seconds).perform_later(@newsletter,@user)
-            # if @users_of_topic
-            #     puts "This user will be notified"
+            # @user=User.find(12)
+            # SendEmailJob.set(wait: 1.seconds).perform_later(@newsletter,@user)
+            if @users_of_topic
+                puts "This user will be notified"
 
-            #     for user in @users_of_topic
-            #         @user = user
-            #         if !notified_user.include?(@user.name)
-            #             notified_user.push(@user.name)
-            #             puts @user.inspect
-            #             SendEmailJob.set(wait: 2.seconds).perform_later(newsletter,@user)
-            #         end
-            #     end
-            # end
+                for user in @users_of_topic
+                    @user = user
+                    if !notified_user.include?(@user.name)
+                        notified_user.push(@user.name)
+                        puts @user.inspect
+                        SendEmailJob.set(wait: 2.seconds).perform_later(newsletter,@user)
+                    end
+                end
+            end
         end
         
     end
